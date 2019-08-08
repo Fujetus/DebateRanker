99dfd24bbde8ce9b2cbd206407757e3fc44ffca8
tarun3trivedi <40719544+tarun3trivedi@users.noreply.github.com>
2019-08-07 21:26:58 -0400

    Add files via upload

--- ritvikbid.py
+++ ritvikbid.py
@@ -24,26 +24,27 @@ while text!= "":
 if text.__len__()== 0:
     print("No Bids")
     exit()
-new_text = f.readline()
-while (new_text.find("&") == -1):
-    text += new_text
+else:
     new_text = f.readline()
+    while (new_text.find("&") == -1):
+        text += new_text
+        new_text = f.readline()
 
-# in a list like 0,1,0,2 we only care about 0 and 0. The fact that they are both 0 means there are two silver bids.
-# A non-zero value indicates a gold bid
-even = True  # this helps switch off every other number
+    # in a list like 0,1,0,2 we only care about 0 and 0. The fact that they are both 0 means there are two silver bids.
+    # A non-zero value indicates a gold bid
+    even = True  # this helps switch off every other number
 
-silver_count = 0;
-gold_count = 0;
-for elem in text:
-    if (elem >= "0" and elem <= "9"):  # if the element we are looking at is a number
-        if even:  # if its one of the 0th, 2nd, 4th, etc element (the ones we care about)
-            if elem == "0":
-                silver_count += 1  # if 0, silver
+    silver_count = 0;
+    gold_count = 0;
+    for elem in text:
+        if (elem >= "0" and elem <= "9"):  # if the element we are looking at is a number
+            if even:  # if its one of the 0th, 2nd, 4th, etc element (the ones we care about)
+                if elem == "0":
+                    silver_count += 1  # if 0, silver
+                else:
+                    gold_count += 1  # if not, gold
+                even = False
             else:
-                gold_count += 1  # if not, gold
-            even = False
-        else:
-            even = True
+                even = True
 
-print("Gold: " + str(gold_count) + " Silver: " + str(silver_count))
+    print("Gold: " + str(gold_count) + " Silver: " + str(silver_count))
